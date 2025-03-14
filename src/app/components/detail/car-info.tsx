2f86425f13f6ca83a48cb0309f03ad3ed756eb0b
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-03-14 20:20:49 +0300

    update

--- src/app/components/detail/car-info.tsx
+++ src/app/components/detail/car-info.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Car } from "@/app/types";
 import { FaHeart, FaStar } from "react-icons/fa";
 import RentButton from "./rent-button";
@@ -46,7 +44,44 @@ const CarInfo = ({ car }: Props) => {
     ];
 
     return (
-        <div>CarInfo</div>
+        <div className="bg-white rounded-lg p-5 flex flex-col justify-between">
+            <div>
+
+                <div>
+                    <h1>{car.make} {car.model}</h1>
+                    <FaHeart className="text-red-500 text-xl" />
+                </div>
+
+                <div className="flex gap-2 my-3 items-center">
+                    {
+                        new Array(5).fill("").map((i, key) => (
+                            <FaStar key={key} className={key < 4 ? "text-yellow-500" : "text-zinc-400"} />
+                        ))
+                    }
+                    <span className="text-zinc-400 text-sm ms-3">440+ yorum</span>
+                </div>
+
+                <div className="grid grid-cols-2 gap-5">
+                    {
+                        arr.map(({ title, value }) => (
+                            <p className="flex justify-between">
+                                <span className="text-gray-400">{title}</span>
+                                <span className="text-zinc-800">{value}</span>
+                            </p>
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className="flex justify-between mt-14">
+                <p>
+                    <span className="text-2xl font-semibold">{car.price}tl /</span>
+                    <span className="ms-1 text-zinc-400">gün</span>
+                </p>
+                <RentButton />
+            </div>
+
+        </div>
     )
 }
 
