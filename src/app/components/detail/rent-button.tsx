a22cd9de879ba525d8ac9f59de0f1cbf72ec0d9b
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-03-16 11:25:14 +0300

    update

--- src/app/components/detail/rent-button.tsx
+++ src/app/components/detail/rent-button.tsx
@@ -1,26 +1,35 @@
-'use client'
+"use client";
 
-import { Car } from "@/app/types"
-import { getPaymentUrl } from "@/app/utils/service"
+import { Car } from "@/app/types";
+import { getPaymentUrl } from "@/app/utils/service";
+import { useState } from "react";
+import Loader from "../loader";
 
 interface Props {
-    car: Car
+    car: Car;
 }
 
-
 const RentButton = ({ car }: Props) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const handleRent = () => {
+        setIsLoading(true);
 
         getPaymentUrl(car)
-            .then((data) => console.log(data))
-    }
+            .then((data) => {
+                window.location.href = data.url;
+            })
+            .finally(() => setIsLoading(false));
+    };
 
     return (
         <button
+            onClick={handleRent}
             className="bg-basic-blue py-2 px-6 text-white rounded-md hover:brightness-90 transition min-w-[150px]"
-            onClick={handleRent}>Şimdi Kirala</button>
-    )
-}
+        >
+            {isLoading ? <Loader size="size-5" /> : "Şimdi Kirala"}
+        </button>
+    );
+};
 
-export default RentButton
+export default RentButton;
